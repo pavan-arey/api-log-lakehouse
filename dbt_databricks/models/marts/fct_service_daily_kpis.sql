8591416b707324a31a62840f3cef0043bb823816
pavan-arey <000hakunamatata@gmail.com>
2026-05-05 04:38:23 +0530

    dbt Gold model

---
+++ dbt_databricks/models/marts/fct_service_daily_kpis.sql
@@ -0,0 +1,38 @@
+with logs as(
+    select * from {{ref('stg_api_logs_clean')}}
+),
+
+svc as(
+    select 
+        service,
+        team,
+        cast(sla_ms as int) as sla_ms,
+        criticality
+    from {{ref('service_catalog')}}
+)
+
+select
+    1.event_date,
+    l.service,
+    s.team,
+    s.criticality,
+    count(*) as total_requests,
+    sum(l.is_client_error) as client_error_requests,
+    sum(l.is_server_error) as server_error_requests,
+    round(
+        1.0 * (sum(l.is_client_error) + sum(l.is_server_error)) / count(*),
+        4
+    ) as error_rate,
+    round(avg(l.latency_ms), 2) as avg_latency_ms,
+    percentile_approx(l.latency_ms, 0.95) as p95_latency_ms,
+    sum(case when l.latency_ms > s.sla_ms then 1 else 0 end) as sla_breach_requests,
+    sum(l.bytes_out) as total_bytes_out
+from logs 1
+left join svc s
+    on l.service = s.service
+
+group by 
+    l.event_date,
+    l.service,
+    s.team,
+    s.criticality
